cba57e4977c2220c77225ecb15bb2c9cf1479a50
sisraily <s.israily@gmail.com>
2020-02-08 11:18:42 -0700

    First commit of lab3_script. Title and user prompt added

---
+++ lab3_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#########################################################
+# Author: Simon Israily
+# Date: 02/08/2020
+# Lab 3
+# Desc.. 
+# 
+#########################################################
+
+printf "Enter a file name: "
+read fileName
+printf "Enter a regex command: "
+read regxcommand
+
+printf "File name ....... $fileName \n"
+printf "Regex Command ... $regexcommand \n"
+
+
+
